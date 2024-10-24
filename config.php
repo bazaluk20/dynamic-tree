bb9d282f2c0972c81492dcc87833a907a3dab571
bazaluk20 <61913436+bazaluk20@users.noreply.github.com>
2024-10-24 19:43:16 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'domsvoim.mysql.tools');     // Database host
-define('DB_NAME', 'domsvoim_tree');          // Database name
-define('DB_USER', 'domsvoim_tree');          // Database user
-define('DB_PASS', '84fN#+aGa8');              // Database password
+define('DB_HOST', 'db-host');       // Database host
+define('DB_NAME', 'db-name');          // Database name
+define('DB_USER', 'db-user');          // Database user
+define('DB_PASS', 'db-pass');              // Database password
 define('DB_CHARSET', 'utf8mb4');    // Database encoding
 
 define('DB_TABLE_PREFIX', 'app_');   // Database table prefix
